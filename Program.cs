6c5fef7daeaea9a8be49229286bf6417d40a0a6d
e.tairov <ev.tairov@yandex.ru>
2023-02-26 16:09:38 +0700

    All

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ string[] InitStringArray(string message)
 
     for (int i = 0; i < n; i++)
     {
-        Console.Write("Введите строку №{0}:", i + 1);
+        Console.Write("Введите строку №{0}: ", i + 1);
         stringArray[i] = Console.ReadLine();
     }
     Console.Write("Вы ввели следующие строки: [");
@@ -36,4 +36,25 @@ string[] InitStringArray(string message)
     return stringArray;
 }
 
+void SelectStrings(string[] stringArray)
+{
+    Console.Write("Новый массив по условию: [");
+
+    for (int i = 0; i < stringArray.Count(); i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            Console.Write(stringArray[i]);
+            if (i < stringArray.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+    Console.Write("]");
+}
+
 string[] stringArray = InitStringArray("");
+Console.WriteLine();
+
+SelectStrings(stringArray);
