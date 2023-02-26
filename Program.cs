ac58e523fac42c1f907204666c1ceff46902cf1f
e.tairov <ev.tairov@yandex.ru>
2023-02-26 16:05:15 +0700

    method InitStringArr

--- Program.cs
+++ Program.cs
@@ -10,3 +10,30 @@
 */
 // -------------------------------------------
 
+string[] InitStringArray(string message)
+{
+    Console.Write("Введите количество строк, которые хотите ввести: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    string[] stringArray = new string[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите строку №{0}:", i + 1);
+        stringArray[i] = Console.ReadLine();
+    }
+    Console.Write("Вы ввели следующие строки: [");
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write(stringArray[i]);
+        if (i < stringArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+
+    return stringArray;
+}
+
+string[] stringArray = InitStringArray("");
